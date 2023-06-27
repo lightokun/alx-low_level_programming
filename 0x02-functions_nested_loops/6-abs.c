cd279269a16e72f620c76931f8b616d9d342caa8
lightokun <sandysllama@yahoo.com>
2023-06-26 19:11:41 -0700

    functions1

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * _abs- gets the absolute of a number
+ * @n: is the number to be checked
+ * Return: the abs of a number.
+ */
+int _abs(int n)
+{
+	if (n >= 0)
+		return (n);
+	return (-n);
+}
